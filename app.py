9a6d97fe53734a940480a287fe482e2814c46d0c
Ivan <tpmrpg@outlook.com>
2022-03-19 11:13:04 -0400

    Added static pages Added basic app route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 # Configure app - Static folder is where images and javascript live
 app = Flask(__name__, static_folder='public', static_url_path='')
@@ -8,3 +8,23 @@ app = Flask(__name__, static_folder='public', static_url_path='')
 @app.route('/')
 def home():
     return render_template("index.html", number=7)
+
+
+@app.route('/shop')
+def shop():
+    return render_template("shop.html")
+
+
+@app.route('/checkout')
+def checkout():
+    return render_template("checkout.html")
+
+
+@app.route('/bag')
+def bag():
+    return render_template("bag.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
